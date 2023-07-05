9d6488bcd426a6e94f988140aa2d655376476820
Cleyton Santana <csttn.dev@gmail.com>
2023-07-04 21:20:37 -0300

    first commit

---
+++ src/main/java/com/coffeeandit/limitessvc/LimitesSvcApplication.java
@@ -0,0 +1,13 @@
+package com.coffeeandit.limitessvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitesSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitesSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coffeeandit/limitessvc/LimitesSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeeandit.limitessvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitesSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
